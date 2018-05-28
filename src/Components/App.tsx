1d532acf45f930d483f943d86d814b09fe6c0f2d
TOMOS EWE <tomosewe@gmail.com>
2018-05-28 19:57:36 +1200

    init create-react-app

---
+++ src/Components/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <React.Fragment>
+        <h1>What movie should I watch?</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;

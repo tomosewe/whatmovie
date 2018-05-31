4647cb66ab860a4a22a035a42692f139841a926a
Tomos Ewe <tomos.ewe@myob.com>
2018-06-01 11:14:41 +1200

    add tmdb attribution

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -2,7 +2,35 @@ import * as React from "react";
 
 class About extends React.Component<{}, {}> {
   public render() {
-    return <p>About</p>;
+    return (
+      <React.Fragment>
+        <p>
+          Created by{" "}
+          <a
+            href="https://tomosewe.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tomos Ewe
+          </a>
+        </p>
+        <a
+          href="https://www.themoviedb.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="images/tmdblogo.png"
+            alt="The Movie DB"
+            style={{ width: "200px" }}
+          />
+        </a>
+        <p>
+          This product uses the TMDb API but is not endorsed or certified by
+          TMDb.{" "}
+        </p>
+      </React.Fragment>
+    );
   }
 }
 

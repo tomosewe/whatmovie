ef108205b32f45e49b693df321feea50e668fcba
tomosewe <tomosewe@gmail.com>
2021-03-05 08:26:47 +1300

    Upgrade fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './Components/App';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./Components/App";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById('root') as HTMLElement
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root") as HTMLElement
 );
 serviceWorker.register();
